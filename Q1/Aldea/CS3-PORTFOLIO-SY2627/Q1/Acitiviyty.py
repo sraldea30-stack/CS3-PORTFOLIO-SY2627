5904a04ca67e0e99ee6a10afbe4a41aa7512e14b
SamuelAldea <sraldea30@src.pshs.edu.ph>
2026-08-11 11:08:02 +0800

    Add files via upload

---
+++ Q1/Aldea/CS3-PORTFOLIO-SY2627/Q1/Acitiviyty.py
@@ -0,0 +1,6 @@
+year = input("Enter year: ")
+if (year % 4) == 0 and (year % 100) == 1:
+    print("Leap year")
+        if 
+else:
+    print("Not a leap year")
---
+++ Q1/Aldea/main.py
@@ -0,0 +1,25 @@
+# math_utils.py
+
+def add_numbers(a, b):
+
+    return a + b
+
+def multiply_numbers(a, b):
+
+    return a * b
+
+def greet_user(name):
+
+    return f"Hello, {name}! Welcome to VS Code."
+
+def main():
+
+    print(add_numbers(15, 10))
+
+    print(multiply_numbers(4, 5))
+
+    print(greet_user("Samuel"))
+
+if __name__ == "__main__":
+
+     main()
